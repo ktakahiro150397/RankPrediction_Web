922ea900fb74c217c4989427536f84b49c180621
Koke Takahiro <k_takahiro_150397@yahoo.co.jp>
2021-08-15 12:52:05 +0900

    各種入力項目・検証アノテーション追加

--- RankPrediction_Web/Models/PredictionDataInputViewModel.cs
+++ RankPrediction_Web/Models/PredictionDataInputViewModel.cs
@@ -14,13 +14,30 @@ namespace RankPrediction_Web.Models
     public class PredictionDataInputViewModel
     {
 
-        [Required(ErrorMessage = "シーズン名を選択してください。")]
+        [Required(ErrorMessage = "{0}を選択してください。")]
         [Display(Name ="シーズン名")]
         public int? SelectedSeasonId { get; set; }
 
         [Required(ErrorMessage = "到達ランクを選択してください。")]
         [Display(Name = "最高到達ランク")]
         public int? SelectedRankId { get; set; }
+
+        [Required(ErrorMessage = "{0}を入力してください。")]
+        [Range(0, 99.99, ErrorMessage = "{0}は{1}から{2}の範囲で入力してください。")]
+        [Display(Name = "K/D")]
+        public decimal? KillDeathRatio { get; set; }
+
+        [Required(ErrorMessage = "{0}を入力してください。")]
+        [Range(0, 99999.99, ErrorMessage = "{0}は{1}から{2}の範囲で入力してください。")]
+        [Display(Name = "平均ダメージ")]
+        public decimal? AverageDamage { get; set; }
+
+        [Required(ErrorMessage = "{0}を入力してください。")]
+        [Display(Name = "合計ゲーム数")]
+        public int? MatchCounts { get; set; }
+
+        [Display(Name = "主にパーティでプレイした")]
+        public bool IsParty { get; set; }
 
         public IEnumerable<SeasonName> SeasonOptions { get; set; }
 
