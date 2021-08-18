8a6a1449a4ec3768f37ffa0b0dc1aa35d5288703
WagashiMgmg <saddler.mw2@gmail.com>
2021-08-18 22:59:40 +0900

    made sql code, it doesn't work

--- Machine_Learning/RankEstimation_model.py
+++ Machine_Learning/RankEstimation_model.py
@@ -7,24 +7,17 @@ from sklearn.svm import SVC
 from sklearn.metrics import hinge_loss
 from sklearn.model_selection import cross_validate, StratifiedKFold, GridSearchCV
 import pickle
-from revoscalepy import RxSqlServerData
-from revoscalepy import rx_import
 
-class mlforrank(object):
+class gen_model(object):
 	"""
 	grid search (cross_validation)でハイパーパラメータ達を決める
-	正答率はmodelを作ったり読み込んだりした後に出す
 	教師データを分割し未知データのほうの正答率がある程度いくまでは例の手順
 	self.classifierはgrid search 用
-	self.clfは答えを返す用
-	grid_enebleがTrueならパラメータを探してからモデルを作る
-	Falseなら保存してあるモデルを読み込む
+	self.clfは答えを返す用.pickleで保存
 	"""
-	def __init__(self, dataframe, grid_eneble):
-		super(mlforrank, self).__init__()
+	def __init__(self, dataframe):
+		super(gen_model, self).__init__()
 		self.dataframe = dataframe
-		self.grid_enable = grid_eneble
-		self.limit = 0.8 #正答率がこれ以下なら入ってきたまま返す
 		self.C = 0.85
 		self.kernel = 'rbf'
 		self.gamma = 0.01
@@ -37,12 +30,6 @@ class mlforrank(object):
 		self.x_test = self.dataframe.iloc[-1,:-1]#dataframeの最後を抜き出す
 		self.y_test = self.dataframe.iloc[-1,-1]
 		#print(self.x_test)
-	def cross_val(self):#cross validationのスコアを返す
-		skf = StratifiedKFold(shuffle=True, random_state=0, n_splits=3)
-		scores = cross_validate(self.clf,self.x,self.y.values.reshape(-1,), cv= skf, return_train_score=True)
-		#print(scores)
-		print(np.mean(scores["test_score"]))
-		return np.mean(scores["test_score"])
 	def grid(self):#ハイパーパラメタの決定
 		tuned_parameters=[{"C":[0.85,1,5,10],"kernel":[self.kernel],"gamma":[1,0.1,0.001,0.0001]}]
 		self.classifier = GridSearchCV(
@@ -56,30 +43,19 @@ class mlforrank(object):
 		self.C = self.classifier.best_params_['C']
 		self.kernel=self.classifier.best_params_['kernel']
 		self.gamma=self.classifier.best_params_['gamma']
-	def estimator(self):
+	def generator(self):
 		self.teature_extractor()
 		self.test_extractor()
-		if self.grid_enable:
-			self.grid()
-			self.clf = SVC(C=self.C, kernel=self.kernel, gamma=self.gamma)
-			with open('model.pickle','wb') as f:
-				pickle.dump(self.clf,f)
-		else:
-			with open('model.pickle','rb') as f:
-				self.clf = pickle.load(f)
-		self.clf.fit(self.x,self.y.values.reshape(-1,))
-		score = self.cross_val()#正答率のようなもの
-		if score < self.limit :
-			return self.y_test
-		else:
-			pred_y = self.classifier.predict(self.x_test.values.reshape([1,-1]))
-			return pred_y[0]
+		self.grid()
+		self.clf = SVC(C=self.C, kernel=self.kernel, gamma=self.gamma)
+		return self.clf
 		
 def main():
 	df=pd.read_csv(r'Machine_Learning\test.csv')
 	print(df)
-	rank = mlforrank(df, True)
-	print(rank.estimator())
-if __name__ == '__main__':
-	main()
+	rank = gen_model(df)
+	clf = rank.generator()
+	with open('model2.pickle','wb') as f:
+		pickle.dump(clf,f)
+main()
 		
