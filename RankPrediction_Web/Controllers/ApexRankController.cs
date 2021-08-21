8ee8dfd325437d3d4fa3a94bf09fab37c71d4dc1
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2021-08-21 17:21:46 +0900

    データ入力部分作成

--- RankPrediction_Web/Controllers/ApexRankController.cs
+++ RankPrediction_Web/Controllers/ApexRankController.cs
@@ -16,22 +16,78 @@ namespace RankPrediction_Web.Controllers
             _context = context;
         }
 
-
-
         public IActionResult Index()
         {
             return View();
         }
 
 
-
+        [HttpGet]
         public IActionResult Input()
         {
-            return View();
+
+            var vm = new PredictionDataInputViewModel
+            {
+                SeasonOptions = _context.SeasonNames,
+                RankOptions = _context.Ranks
+            };
+
+            return View(vm);
         }
 
-        public IActionResult Result()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Input([Bind("SelectedSeasonId," +
+            "SelectedRankId",
+            "KillDeathRatio",
+            "AverageDamage",
+            "MatchCounts",
+            "IsParty")] PredictionDataInputViewModel bindVm)
         {
+            if (ModelState.IsValid)
+            {
+                var addPrediction = new PredictionDatum()
+                {
+                    SeasonId = bindVm.SelectedSeasonId.Value,
+                    RankId = bindVm.SelectedRankId.Value,
+                    KillDeathRatio = bindVm.KillDeathRatio.Value,
+                    AverageDamage = bindVm.AverageDamage.Value,
+                    IsParty = bindVm.IsParty,
+                    MatchCounts = bindVm.MatchCount_long
+                };
+
+                _context.Add(addPrediction);
+                await _context.SaveChangesAsync();
+
+                //インサートしたデータのIDを取得
+                var insertId = addPrediction.Id;
+
+                return RedirectToAction("Result", "ApexRank",new { id = insertId });
+
+            }
+            else
+            {
+                var vm = new PredictionDataInputViewModel
+                {
+                    SeasonOptions = _context.SeasonNames,
+                    RankOptions = _context.Ranks
+                };
+
+                return View(vm);
+            }
+        }
+
+        public IActionResult Result(int? id)
+        {
+
+            //URLパラメータが存在しない場合トップに戻る
+            if(!id.HasValue)
+            {
+                return View("Index", "ApexRank");
+            }
+
+
+
             return View();
         }
 
