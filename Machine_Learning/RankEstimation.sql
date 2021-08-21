dd7b31c09f160ea3f9094263ac220291b63c45ba
WagashiMgmg <saddler.mw2@gmail.com>
2021-08-22 01:03:31 +0900

    issue around "id"

--- Machine_Learning/RankEstimation.sql
+++ Machine_Learning/RankEstimation.sql
@@ -40,17 +40,13 @@ class gen_model(object):
 		super(gen_model, self).__init__()
 		self.dataframe = dataframe
 		self.C = 0.85
-		self.kernel = 'rbf'
+		self.kernel = "rbf"
 		self.gamma = 0.01
 	def teature_extractor(self):
-		self.x = self.dataframe[self.dataframe['id'] != self.id].iloc[:,1:-1]#dataframeの最後のcol以外を説明変数としている
+		self.x = self.dataframe.iloc[:,1:-1]#dataframeの最後のcol以外を説明変数としている
 		print(self.x)
-		self.y = self.dataframe[self.dataframe['id'] != self.id].iloc[:,[-1]]#最後のcolはランクラベル
+		self.y = self.dataframe.iloc[:,[-1]]#最後のcolはランクラベル
 		print(self.y)
-	def test_extractor(self):
-		self.x_test = self.dataframe[self.dataframe['id'] == self.id].iloc[-1,1:-1]#dataframeの最後を抜き出す
-		self.y_test = self.dataframe[self.dataframe['id'] == self.id].iloc[-1,-1]
-		print(self.x_test)
 	def grid(self):#ハイパーパラメタの決定
 		tuned_parameters=[{"C":[0.85,1,5,10],"kernel":[self.kernel],"gamma":[1,0.1,0.001,0.0001]}]
 		self.classifier = GridSearchCV(
@@ -61,12 +57,11 @@ class gen_model(object):
 		self.classifier.fit(self.x,self.y.values.reshape(-1,))
 		#print(self.classifier.cv_results_)
 		#print(self.classifier.best_params_)
-		self.C = self.classifier.best_params_['C']
-		self.kernel=self.classifier.best_params_['kernel']
-		self.gamma=self.classifier.best_params_['gamma']
+		self.C = self.classifier.best_params_["C"]
+		self.kernel=self.classifier.best_params_["kernel"]
+		self.gamma=self.classifier.best_params_["gamma"]
 	def generator(self):
 		self.teature_extractor()
-		self.test_extractor()
 		self.grid()
 		self.clf = SVC(C=self.C, kernel=self.kernel, gamma=self.gamma)
 		return self.clf
@@ -122,19 +117,23 @@ class mlforrank(object):
 	self.clfは答えを返す用
 
 	"""
-	def __init__(self, dataframe):
+	def __init__(self, dataframe, id):
 		super(mlforrank, self).__init__()
 		self.dataframe = dataframe
-		self.limit = 0.8
+		self.id = id #入力したユーザーのid
+		self.limit = 0.8 #正答率がこれ以下なら入ってきたまま返す
+		self.C = 0.85
+		self.kernel = "rbf"
+		self.gamma = 0.01
 	def teature_extractor(self):
-		self.x = self.dataframe.iloc[:-1,1:-1]#dataframeの最後のcol以外を説明変数としている#最初のcolはデータのidなので落とす
-		#print(self.x)
-		self.y = self.dataframe.iloc[:-1,[-1]]#最後のcolはランクラベル
-		#print(self.y)
+		self.x = self.dataframe[self.dataframe["id"] != self.id].iloc[:,1:-1]#dataframeの最後のcol以外を説明変数としている
+		print(self.x)
+		self.y = self.dataframe[self.dataframe["id"] != self.id].iloc[:,[-1]]#最後のcolはランクラベル
+		print(self.y)
 	def test_extractor(self):
-		self.x_test = self.dataframe.iloc[-1,:-1]#dataframeの最後を抜き出す
-		self.y_test = self.dataframe.iloc[-1,-1]
-		#print(self.x_test)
+		self.x_test = self.dataframe[self.dataframe["id"] == self.id].iloc[-1,1:-1]#dataframeの最後を抜き出す
+		self.y_test = self.dataframe[self.dataframe["id"] == self.id].iloc[-1,-1]
+		print(self.x_test)
 	def cross_val(self):#cross validationのスコアを返す
 		skf = StratifiedKFold(shuffle=True, random_state=0, n_splits=3)
 		scores = cross_validate(self.clf,self.x,self.y.values.reshape(-1,), cv= skf, return_train_score=True)
@@ -168,8 +167,9 @@ class mlforrank(object):
 		
 def estimate():
 	df=rank_score_data
+	id= #yamaimo頼んだ
 	print(df)
-	rank = mlforrank(df)
+	rank = mlforrank(df, id)
 	rank_id = rank.estimator()
 
 estimate()
