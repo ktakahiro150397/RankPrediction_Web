7286526e0277c78f8fff6f3ef0ca11154c73cadd
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2021-08-23 23:22:24 +0900

    ロードアイコン追加

---
+++ RankPrediction_Web/wwwroot/js/loading.js
@@ -0,0 +1,23 @@
+﻿document.onreadystatechange = function () {
+    const loader = document.getElementById("load-icon");
+    var state = document.readyState
+    if (state == 'interactive') {
+        loader.classList.remove("icon-disable");
+    } else if (state == 'loading') {
+        loader.classList.remove("icon-disable");
+    } else {
+        loader.classList.add("icon-disable");
+    }
+
+    console.log("state:" + state);
+}
+
+document.onsubmit = function () {
+    const loader = document.getElementById("load-icon");
+    loader.classList.remove("icon-disable");
+}
+
+window.onbeforeunload = function () {
+    const loader = document.getElementById("load-icon");
+    loader.classList.remove("icon-disable");
+}
