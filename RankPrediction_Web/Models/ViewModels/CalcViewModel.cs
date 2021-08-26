c74da0d025831310008e56590eed75e19d79accc
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2021-08-26 23:08:25 +0900

    名言VM追加

--- RankPrediction_Web/Models/ViewModels/CalcViewModel.cs
+++ RankPrediction_Web/Models/ViewModels/CalcViewModel.cs
@@ -10,8 +10,12 @@ namespace RankPrediction_Web.Models.ViewModels
         /// <summary>
         /// 結果表示を要求する入力データID
         /// </summary>
-        public int DataId {get;set;}
+        public int? DataId {get;set;}
 
+        /// <summary>
+        /// 画面に表示する名言
+        /// </summary>
+        public string Saying { get; set; }
 
     }
 }
