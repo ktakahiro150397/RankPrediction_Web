d94da2594c37c0546e317b15a870e845fbc8af98
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2021-08-27 21:37:06 +0900

    天才ボタン

--- RankPrediction_Web/wwwroot/js/toggleNativeShareButton.js
+++ RankPrediction_Web/wwwroot/js/toggleNativeShareButton.js
@@ -14,6 +14,6 @@ if (navigator.share) {
     console.log("navigator.share is not supported!");
 
     //画面のネイティブ共有ボタンを非表示にする(shareLink属性がついているタグを全て非表示にする)
-    var shareLinks = Array.prototype.slice.call(document.getElementsByTagName("a"));
-    shareLinks.filter(item => item.hasAttribute("shareLink")).forEach(item => item.style.display = "none");
+    //var shareLinks = Array.prototype.slice.call(document.getElementsByTagName("a"));
+    //shareLinks.filter(item => item.hasAttribute("shareLink")).forEach(item => item.style.display = "none");
 }
