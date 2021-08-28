4a3d17cea9d8c361a6c9d9d241aca9ff8f9db3d2
WagashiMgmg <saddler.mw2@gmail.com>
2021-08-28 19:08:11 +0900

    merge

--- Machine_Learning/RankEstimation.py
+++ Machine_Learning/RankEstimation.py
@@ -21,21 +21,12 @@ class mlforrank(object):
 	grid_enebleがTrueならパラメータを探してからモデルを作る
 	Falseなら保存してあるモデルを読み込む
 	"""
-<<<<<<< HEAD
 	def __init__(self, dataframe, id):
 		super(mlforrank, self).__init__()
 		self.dataframe = dataframe
 		self.id = id #入力したユーザーのid
 		self.limit = 0.8 #正答率がこれ以下なら入ってきたまま返す
 		self.nsplit = 3 #cvとかgridとかでデータをスプリットする個数
-=======
-	def __init__(self, dataframe, id, grid_eneble):
-		super(mlforrank, self).__init__()
-		self.dataframe = dataframe
-		self.id = id #入力したユーザーのid
-		self.grid_enable = grid_eneble
-		self.limit = 0.8 #正答率がこれ以下なら入ってきたまま返す
->>>>>>> 0f29b05c3d9d820bf776e891b715ada3a10316d2
 		self.C = 0.85
 		self.kernel = "rbf"
 		self.gamma = 0.01
@@ -51,7 +42,6 @@ class mlforrank(object):
 		self.unique = len(self.dfbool)<=1 #計算するidのランクがそいつだけしか無いか.T/F
 		print(self.unique)
 	def teature_extractor(self):
-<<<<<<< HEAD
 		self.x = self.dataframe[self.dataframe["id"] != self.id].iloc[:,1:-1]#dataframeの最後のcol以外を説明変数としている
 		print(self.x)
 		self.y = self.dataframe[self.dataframe["id"] != self.id].iloc[:,[-1]]#最後のcolはランクラベル
@@ -61,18 +51,6 @@ class mlforrank(object):
 		self.y_test = self.userrank
 	def cross_val(self):#cross validationのスコアを返す
 		skf = StratifiedKFold(shuffle=True, random_state=0, n_splits=self.nsplit)
-=======
-		self.x = self.dataframe[self.dataframe['id'] != self.id].iloc[:,1:-1]#dataframeの最後のcol以外を説明変数としている
-		print(self.x)
-		self.y = self.dataframe[self.dataframe['id'] != self.id].iloc[:,[-1]]#最後のcolはランクラベル
-		print(self.y)
-	def test_extractor(self):
-		self.x_test = self.dataframe[self.dataframe['id'] == self.id].iloc[-1,1:-1]#dataframeの最後を抜き出す
-		self.y_test = self.dataframe[self.dataframe['id'] == self.id].iloc[-1,-1]
-		print(self.x_test)
-	def cross_val(self):#cross validationのスコアを返す
-		skf = StratifiedKFold(shuffle=True, random_state=0, n_splits=3)
->>>>>>> 0f29b05c3d9d820bf776e891b715ada3a10316d2
 		scores = cross_validate(self.clf,self.x,self.y.values.reshape(-1,), cv= skf, return_train_score=True)
 		#print(scores)
 		print(np.mean(scores["test_score"]))
@@ -93,11 +71,7 @@ class mlforrank(object):
 		self.kernel=self.classifier.best_params_['kernel']
 		self.gamma=self.classifier.best_params_['gamma']
 	def weight(self,unestimation):
-<<<<<<< HEAD
 		d = [(0, 98), (1, 844), (2, 1028)]
-=======
-		d = [(-1, 98), (0, 844), (1, 1028)]
->>>>>>> 0f29b05c3d9d820bf776e891b715ada3a10316d2
 		a, w = zip(*d)
 		#print(a, w)
 		w2 = np.array(w) / sum(w)
@@ -109,20 +83,13 @@ class mlforrank(object):
 		print(Counter(c))
 		"""
 		result = unestimation+v
-<<<<<<< HEAD
 		if result>=22:
 			result = 21
 		elif result<=-1:
-=======
-		if result==22:
-			result = 21
-		elif result==-1:
->>>>>>> 0f29b05c3d9d820bf776e891b715ada3a10316d2
 			result = 0
 		return result
 		
 	def estimator(self):
-<<<<<<< HEAD
 		if self.unique:
 			print(self.unique)
 			return self.weight(self.userrank)#重みのついたランダムを返す
@@ -151,34 +118,11 @@ class mlforrank(object):
 				pred_y = self.clf.predict(self.x_test.values.reshape([1,-1]))
 				print("predicted")
 				return pred_y[0]
-=======
-		self.teature_extractor()
-		self.test_extractor()
-		if self.grid_enable:
-			self.grid()
-			self.clf = SVC(C=self.C, kernel=self.kernel, gamma=self.gamma)
-			with open('model.pickle','wb') as f:
-				pickle.dump(self.clf,f)
-		else:
-			with open('model.pickle','rb') as f:
-				self.clf = pickle.load(f)
-		self.clf.fit(self.x,self.y.values.reshape(-1,))
-		score = self.cross_val()#正答率のようなもの
-		if score < self.limit :
-			return self.weight(self.y_test)#重みのついたランダムを返す
-		else:
-			pred_y = self.classifier.predict(self.x_test.values.reshape([1,-1]))
-			return pred_y[0]
->>>>>>> 0f29b05c3d9d820bf776e891b715ada3a10316d2
 		
 def main():
 	df=pd.read_csv(r'Machine_Learning\test.csv')
 	print(df)
-<<<<<<< HEAD
 	rank = mlforrank(df, 6)
-=======
-	rank = mlforrank(df, 2,True)
->>>>>>> 0f29b05c3d9d820bf776e891b715ada3a10316d2
 	print(rank.estimator())
 if __name__ == '__main__':
 	main()
