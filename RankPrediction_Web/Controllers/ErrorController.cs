d802788fb7bead4b862c7a1912ac4481aea29d44
Koke Takahiro <k_takahiro_150397@yahoo.co.jp>
2021-08-29 17:01:32 +0900

    エラーページ名言追加

--- RankPrediction_Web/Controllers/ErrorController.cs
+++ RankPrediction_Web/Controllers/ErrorController.cs
@@ -3,14 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RankPrediction_Web.Models.DbContexts;
+using RankPrediction_Web.Models.ViewModels;
 
 namespace RankPrediction_Web.Controllers
 {
     public class ErrorController : Controller
     {
+        private RankPredictionContext db;
+
+        public ErrorController(RankPredictionContext context)
+        {
+            db = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+
+            var vm = new ErrorViewModel(db);
+
+            return View(vm);
         }
     }
 }
--- RankPrediction_Web/Models/ViewModels/ErrorViewModel.cs
+++ RankPrediction_Web/Models/ViewModels/ErrorViewModel.cs
@@ -1,11 +1,42 @@
 using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using RankPrediction_Web.Models.DbContexts;
 
-namespace RankPrediction_Web.Models
+namespace RankPrediction_Web.Models.ViewModels
 {
     public class ErrorViewModel
     {
+        public ErrorViewModel(RankPredictionContext db)
+        {
+
+            Saying = db.Sayings.FromSqlRaw("SELECT TOP(1) * FROM [ml_predict].[sayings] ORDER BY NEWID() ")
+                    .First();
+        }
+
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+
+        public Saying Saying { get; set; }
+
+        public string SayingStr
+        {
+            get
+            {
+                return $"\"{Saying.SayingJa}\"";
+            }
+        }
+
+        public string SayingByStr
+        {
+            get
+            {
+                return $"\"{Saying.SayingByJa}\"";
+            }
+        }
+
+
     }
 }
