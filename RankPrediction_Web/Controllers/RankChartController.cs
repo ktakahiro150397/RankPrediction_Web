e9480f904ef68c310a5be4af41f963bd32bf9a51
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2021-08-31 00:43:18 +0900

    サンプルのデータ表示

--- RankPrediction_Web/Controllers/RankChartController.cs
+++ RankPrediction_Web/Controllers/RankChartController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RankPrediction_Web.Models.ViewModels.Chart;
+using RankPrediction_Web.Models.DbContexts;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,20 @@ namespace RankPrediction_Web.Controllers
 {
     public class RankChartController : Controller
     {
+
+        private readonly RankPredictionContext _context;
+
+        public RankChartController(RankPredictionContext context)
+        {
+            _context = context;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+
+            var vm = new ChartIndexViewModel(_context);
+            return View(vm);
         }
     }
 }
--- RankPrediction_Web/Models/ViewModels/Chart/ChartIndexViewModel.cs
+++ RankPrediction_Web/Models/ViewModels/Chart/ChartIndexViewModel.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using RankPrediction_Web.Models.DbContexts;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
 namespace RankPrediction_Web.Models.ViewModels.Chart
 {
-    public class ChartIndexViewModel
+    public class ChartIndexViewModel : LayoutViewModel
     {
-        public ChartIndexViewModel()
+        public ChartIndexViewModel(RankPredictionContext db) : base()
         {
+
+            RankAverageKillRatio = new ChartData();
+
+            var rankAverageKills =
+               db.PredictionData
+               .GroupBy(item => item.RankId)
+               .OrderBy(item => item.Key)
+               .Select(item => new { RankId = item.Key, KillRatioAverage = item.Average(elem => elem.KillDeathRatio) })
+               .ToList();
+
+
+            foreach(var elem in rankAverageKills)
+            {
+                RankAverageKillRatio.data.Add(elem.KillRatioAverage.Value);
+            }
+
         }
+
+        /// <summary>
+        /// ランクと平均キルレシオのチャートデータクラス
+        /// </summary>
+        public ChartData RankAverageKillRatio { get; set; }
+
+
     }
 }
