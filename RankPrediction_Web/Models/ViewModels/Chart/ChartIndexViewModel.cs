04755ba504d08a1000098c47e7bdeb970df3435e
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2021-08-30 20:30:21 +0900

    チャートページ雛形

---
+++ RankPrediction_Web/Models/ViewModels/Chart/ChartIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RankPrediction_Web.Models.ViewModels.Chart
+{
+    public class ChartIndexViewModel
+    {
+        public ChartIndexViewModel()
+        {
+        }
+    }
+}
