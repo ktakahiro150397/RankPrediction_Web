3889a0e6dae6622ed926af5b690f6e675faaa7d5
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2021-08-31 23:22:24 +0900

    DBコンテキスト更新

---
+++ RankPrediction_Web/Models/DbContexts/RanksGeneral.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace RankPrediction_Web.Models.DbContexts
+{
+    public partial class RanksGeneral
+    {
+        public RanksGeneral()
+        {
+            RankAmazonUrls = new HashSet<RankAmazonUrl>();
+        }
+
+        public int Id { get; set; }
+        public string RankName { get; set; }
+
+        public virtual ICollection<RankAmazonUrl> RankAmazonUrls { get; set; }
+    }
+}
