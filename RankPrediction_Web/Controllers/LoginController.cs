38525bc3cd9b1c479d129a1da29346008a6016dc
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2021-09-11 14:39:57 +0900

    外部ログインページ・パッケージ追加

---
+++ RankPrediction_Web/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RankPrediction_Web.Models.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RankPrediction_Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(new LayoutViewModel());
+        }
+
+
+        
+        public IActionResult SteamLogin()
+        {
+            return View(new LayoutViewModel());
+        }
+
+
+    }
+}
