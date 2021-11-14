64c6371a60e4d12d473175f67092eb019c1bc8ab
WagashiMgmg <saddler.mw2@gmail.com>
2021-11-14 21:01:22 +0900

    データの標準化

--- Machine_Learning/RankEstimation.py
+++ Machine_Learning/RankEstimation.py
@@ -5,11 +5,15 @@
 #modelをつくるかの true false
 """
 
+import pickle
+
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import (GridSearchCV, StratifiedKFold,
+                                     cross_validate)
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.svm import SVC
-from sklearn.model_selection import cross_validate, StratifiedKFold, GridSearchCV
-import pickle
+
 
 class mlforrank(object):
 	"""
@@ -25,47 +29,56 @@ class mlforrank(object):
 		super(mlforrank, self).__init__()
 		self.dataframe = dataframe
 		self.id = id #入力したユーザーのid
-		self.limit = 0.1 #正答率がこれ以下なら入ってきたまま返す
-		self.nsplit = 2 #cvとかgridとかでデータをスプリットする個数
-		self.C = 1000
-		self.kernel = "rbf"
-		self.gamma = "scale"
 		self.q1 = 0.04 #下側の外れ値をどれだけ省くか
 		self.q2 = 0.92 #上側の外れ値をどれだけ省くか。上位 (1-q2)*100 % のデータを外れ値として省く
+		rank_id_list=self.dataframe["rank_id"].unique()
+		for i in self.dataframe.columns.values[1:-2]:
+			for rank in rank_id_list:
+				self.flier(i, rank)
+		self.standardizatoin_normalization()
+		self.limit = 0.1 #正答率がこれ以下なら入ってきたまま返す
+		self.nsplit = 2 #cvとかgridとかでデータをスプリットする個数
+		self.C = 10000
+		self.kernel = "rbf"
+		self.gamma = "scale"
 		self.userrank = self.dataframe[self.dataframe["id"] == self.id].iloc[-1,-1]#計算するidのランク
 		self.norank = self.userrank == 22
 		self.rank_col_name = self.dataframe.columns.values[-1]#ランクが入っているcolの名前
 		print("rank conuts")
 		print(self.dataframe[self.rank_col_name].value_counts())
 		self.number_of_members_in_each_rank = self.dataframe[self.rank_col_name].value_counts().min()#データベースに入っているランクの中で一番少ないやつの頻度
-		self.do_grid = self.number_of_members_in_each_rank > self.nsplit + 10 #gridするか否か.T/F.+している数値はサンプル数に応じ変更する
+		self.do_grid = self.number_of_members_in_each_rank > self.nsplit + 1 #gridするか否か.T/F.+している数値はサンプル数に応じ変更する
 		self.do_split = self.number_of_members_in_each_rank > self.nsplit
 		self.do_est = self.number_of_members_in_each_rank >= self.nsplit
 		self.dfbool = self.dataframe[self.dataframe[self.rank_col_name] == self.userrank]#計算するidのランクと一致するrowのみで構成されるdf
 		self.unique = len(self.dfbool)<=1 #計算するidのランクがそいつだけしか無いか.T/F
 		print(self.unique)
+	def standardizatoin_normalization(self):
+		sc = StandardScaler()
+		new_cols = [col for col in self.dataframe.columns if not (col == "id" or col == "rank_id" or col == "is_party")]
+		def_cols = [col for col in self.dataframe.columns if col == "id" or col == "rank_id" or col == "is_party"]
+		std = sc.fit_transform(self.dataframe[new_cols])
+		dfa = pd.DataFrame(std, columns=new_cols)
+		dfb = pd.concat([self.dataframe[def_cols].reset_index(drop=True), dfa], axis = 1)
+		self.dataframe = dfb.reindex(columns = self.dataframe.columns)
+		print(self.dataframe.min())
 	def teature_extractor(self):
-		rank_id_list=self.dataframe['rank_id'].unique()
-		for i in self.dataframe.columns.values[1:-2]:
-			for rank in rank_id_list:
-				self.flier(i, rank)
 		print("外れ値削除後")
 		print(self.dataframe)
 		self.x = self.dataframe[self.dataframe["id"] != self.id].iloc[:,1:-1]#dataframeの最後のcol以外を説明変数としている
-		print(self.x)
 		self.y = self.dataframe[self.dataframe["id"] != self.id].iloc[:,[-1]]#最後のcolはランクラベル
-		print(self.y)
-	def flier(self,key, rank):#外れ値削除
-		dfa = self.dataframe[self.dataframe["rank_id"]==rank]
+	def flier(self,key, rank):#指定id以外において外れ値削除
+		dfa = self.dataframe[self.dataframe["rank_id"]==rank]#外れ値はランクごとに計算する
 		q1 = dfa[key].quantile(self.q1)
 		q2 = dfa[key].quantile(self.q2)
 		max = q2
 		min = q1
-		self.dataframe = self.dataframe[(self.dataframe["rank_id"]!=rank) | ((self.dataframe[key] <= max) & (self.dataframe["rank_id"]==rank))]
-		self.dataframe = self.dataframe[(self.dataframe["rank_id"]!=rank) | ((self.dataframe[key] >= min) & (self.dataframe["rank_id"]==rank))]
+		self.dataframe = self.dataframe[(self.dataframe["id"] == self.id) | (self.dataframe["rank_id"]!=rank) | ((self.dataframe[key] <= max) & (self.dataframe["rank_id"]==rank))]
+		self.dataframe = self.dataframe[(self.dataframe["id"] == self.id) | (self.dataframe["rank_id"]!=rank) | ((self.dataframe[key] >= min) & (self.dataframe["rank_id"]==rank))]
 	def test_extractor(self):
 		self.x_test = self.dataframe[self.dataframe["id"] == self.id].iloc[-1,1:-1]#dataframeの最後を抜き出す
 		self.y_test = self.userrank
+		print(self.x_test)
 	def cross_val(self):#cross validationのスコアを返す
 		skf = StratifiedKFold(shuffle=True, random_state=0, n_splits=self.nsplit)
 		scores = cross_validate(self.clf,self.x,self.y.values.reshape(-1,), cv= skf, return_train_score=True)
@@ -75,11 +88,12 @@ class mlforrank(object):
 	def fitting_score(self):#モデルで教師データを学習した時のスコアを返す
 		return self.clf.score(self.x, self.y)
 	def grid(self):#ハイパーパラメタの決定
-		tuned_parameters=[{"C":[0.85,1,5,10],"kernel":[self.kernel],"gamma":[1,0.1,0.001,0.0001]}]
+		tuned_parameters=[{"C":[0.85,1,10,100,1000,10000],"kernel":[self.kernel],"gamma":["scale",1,0.1,0.001,0.0001]}]
 		self.classifier = GridSearchCV(
 			SVC(),
 			tuned_parameters,
 			cv=self.nsplit,
+			scoring='neg_mean_absolute_error'
 		)
 		self.classifier.fit(self.x,self.y.values.reshape(-1,))
 		#print(self.classifier.cv_results_)
@@ -145,13 +159,18 @@ class mlforrank(object):
 			return self.weight(self.userrank)#重みのついたランダムを返す
 		else:
 			pred_y = self.clf.predict(self.x_test.values.reshape([1,-1]))
+			all_pred = self.clf.predict(self.x).astype(np.int8)
+			self.y = self.y.astype(np.int8)
+			abs_diff = [np.abs(n-m) for n,m in zip(all_pred, self.y.values.reshape(-1,).astype(np.int8))]
 			print("predicted")
+			print("score: {}".format(score))
+			print("ave diff:{}".format(np.average(abs_diff)))
 			return pred_y[0]
 
 def main():
 	df=pd.read_csv(r'Machine_Learning\1114.csv', )
 	print(df)
-	rank = mlforrank(df, 1204)
+	rank = mlforrank(df, 9)
 	print(rank.estimator())
 if __name__ == '__main__':
 	main()
